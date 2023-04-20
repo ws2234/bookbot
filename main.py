60ec67d6382d5c44145389d111d33f99181fde39
Wesley Strange <wesley.ws.strange@gmail.com>
2023-04-20 10:25:26 -0700

    Updated main.py: Add function to count the letters in input text file

--- main.py
+++ main.py
@@ -2,8 +2,23 @@ def count_words(contents):
     words = contents.split()
     print(len(words))
 
+def count_letters(contents):
+    letter_counts = {}
+
+    words = contents.split()
+    for word in words:
+        for letter in word:
+            if letter.lower() in letter_counts:
+                letter_counts[letter.lower()] += 1
+            else:
+                letter_counts[letter.lower()] = 1
+
+    print(letter_counts)
+
 with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     print(file_contents)
 
 count_words(file_contents)
+
+count_letters(file_contents)
